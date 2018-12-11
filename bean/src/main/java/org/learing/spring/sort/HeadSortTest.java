f74d5ede34342dd72f7ea21a84ea7bbb20127c66
yang.chen <jdiop417@gmail.com>
2018-12-11 17:40:58 +0800

    添加堆排序

---
+++ bean/src/main/java/org/learing/spring/sort/HeadSortTest.java
@@ -0,0 +1,48 @@
+package org.learing.spring.sort;
+
+import java.util.Arrays;
+
+public class HeadSortTest {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int[] arr) {
+        //构建堆
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(arr, i, arr.length);
+        }
+
+        //排序
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            adjustHeap(arr, 0, i);
+        }
+
+    }
+
+    private static void adjustHeap(int[] arr, int i, int length) {
+        int tmp = arr[i];
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
+                k++;
+            }
+            if (arr[k] > tmp) {
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = tmp;
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
