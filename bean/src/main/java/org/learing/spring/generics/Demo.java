55741df4d162a32b16b2040c5add297dcace88b7
yang.chen <jdiop417@gmail.com>
2019-02-07 13:06:04 +0800

    添加注释

--- bean/src/main/java/org/learing/spring/generics/Demo.java
+++ bean/src/main/java/org/learing/spring/generics/Demo.java
@@ -8,9 +8,19 @@ import java.util.ArrayList;
 
 public class Demo {
 
+    /**
+     * 把字符串变成数字集合
+     *
+     * @param sourStr     字符串
+     * @param split       字符串分隔符
+     * @param targetClass 数字类型，eg：integr，Long
+     * @return 数字集合
+     */
     private static <T extends Number> ArrayList<T> parseNumberList(String sourStr, String split, Class<T> targetClass) {
-        ArrayList list = Lists.newArrayList();
+        if (StringUtils.isEmpty(sourStr)) return new ArrayList<>();
+
         String[] strs = sourStr.split(split);
+        ArrayList list = Lists.newArrayListWithCapacity(strs.length);
         for (String str : strs) {
             if (StringUtils.isEmpty(str)) continue;
             list.add(NumberUtils.parseNumber(str, targetClass));
@@ -20,7 +30,7 @@ public class Demo {
 
 
     public static void main(String[] args) {
-        String text = "1,2,2,3,3,3,";
+        String text = null;
         ArrayList<Long> result = parseNumberList(text, ",", Long.class);
         for (Long tmp : result) {
             System.out.println(tmp);
