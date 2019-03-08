fd602600cbb1e62bd4ac07944e7610a4c21d283f
yang.chen <jdiop417@gmail.com>
2019-03-08 16:30:16 +0800

    提交aop测试类

---
+++ bean/src/main/java/org/learing/spring/aop/Test/Config.java
@@ -0,0 +1,7 @@
+package org.learing.spring.aop.Test;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(value = {"org.learing.spring.aop.Test"})
+public class Config {
+}
