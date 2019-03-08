77cc2416debf5322a369c80c54f38b411533a993
yang.chen <jdiop417@gmail.com>
2019-03-08 16:30:16 +0800

    提交aop测试类

---
+++ foundation/src/main/java/org/learning/foundation/aop/Test/Config.java
@@ -0,0 +1,20 @@
+package org.learning.foundation.aop.Test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@ComponentScan(value = {"org.learing.spring.aop.Test"})
+public class Config {
+
+    @Bean
+    public MathCalculator mathCalculator() {
+        return new MathCalculator();
+    }
+
+    @Bean
+    public LogAspects logAspects() {
+        return new LogAspects();
+    }
+}
