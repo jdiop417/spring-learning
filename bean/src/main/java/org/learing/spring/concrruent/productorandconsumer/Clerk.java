70a73783ecddb0884bb591336b5096bf0e4d82bb
yang.chen <jdiop417@gmail.com>
2019-09-09 20:48:28 +0800

    feature:添加生产者消费者案例

---
+++ bean/src/main/java/org/learing/spring/concrruent/productorandconsumer/Clerk.java
@@ -0,0 +1,31 @@
+package org.learing.spring.concrruent.productorandconsumer;
+
+/**
+ * 员工
+ */
+public class Clerk {
+
+    private static final int MAX_PRODUCT_NUM = 1;
+
+    private int product = 0;
+
+    // 买
+    public synchronized void get() throws InterruptedException {
+        while (product >= MAX_PRODUCT_NUM) {
+            System.out.println("产品已满！");
+            this.wait();
+        }
+        System.out.println("产品数量：" + ++product);
+        this.notifyAll();
+    }
+
+    //卖
+    public synchronized void sale() throws InterruptedException {
+        while (product <= 0) {
+            System.out.println("产品缺货！");
+            this.wait();
+        }
+        System.out.println("产品数量：" + --product);
+        this.notifyAll();
+    }
+}
