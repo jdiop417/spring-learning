bf0c9d5b4503f37665477e17ae230a1f2517287e
yang.chen <jdiop417@gmail.com>
2019-12-25 17:07:55 +0800

    提交代码

--- springboot-cache/src/main/java/org/learning/springbootcache/controller/EmployeeController.java
+++ springboot-cache/src/main/java/org/learning/springbootcache/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package org.learning.springbootcache.controller;
 import org.learning.springbootcache.bean.Employee;
 import org.learning.springbootcache.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +14,27 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping("/emp/{id}")
-    @Cacheable(cacheNames = "emp", keyGenerator = "mykeyGenerator", condition = "#id>0")
     public Employee getEmp(@PathVariable("id") Integer id) {
         return employeeService.getEmpById(id);
     }
+
+
+    @GetMapping("/emp")
+    public Employee updateEmp(Employee employee) {
+        employeeService.updateEmp(employee);
+        return employee;
+    }
+
+
+    @GetMapping("/delemp")
+    public String deleteEmp(Integer id) {
+        employeeService.deleteEmp(id);
+        return "success";
+    }
+
+    @GetMapping("/emp/lastName/{lastName}")
+    public Employee getEmpByLastName(@PathVariable("lastName") String lastName) {
+        return employeeService.getEmpByLastName(lastName);
+    }
+
 }
--- springboot-cache/src/main/java/org/learning/springbootcache/service/EmployeeService.java
+++ springboot-cache/src/main/java/org/learning/springbootcache/service/EmployeeService.java
@@ -2,17 +2,52 @@ package org.learning.springbootcache.service;
 
 import org.learning.springbootcache.bean.Employee;
 import org.learning.springbootcache.mapper.EmployeeMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
 
 @Service
 public class EmployeeService {
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Resource
     private EmployeeMapper employeeMapper;
 
+    @Cacheable(cacheNames = "emp", key = "#id", condition = "#id>0")
     public Employee getEmpById(Integer id) {
-        System.out.println("查询" + id + "号员工");
+        logger.debug("查询" + id + "号员工");
         return employeeMapper.getEmpById(id);
+    }
+
+    @CachePut(value = "emp", key = "#result.id")
+    public Employee updateEmp(Employee employee) {
+        logger.debug("更新" + employee.getId() + "号员工");
+        employeeMapper.updateEmp(employee);
+        return employee;
+    }
+
+    @CacheEvict(value = "emp", key = "#id", beforeInvocation = true)
+    public void deleteEmp(@NotNull Integer id) {
+        logger.debug("删除" + id + "号员工");
+        employeeMapper.deletEmpById(id);
+    }
+
+
+    @Caching(
+            cacheable = {@Cacheable(value = "emp", key = "#lastname")},
+            put = {
+                    @CachePut(value = "emp", key = "#result.id"),
+                    @CachePut(value = "emp", key = "#result.email")
+            }
+    )
+    public Employee getEmpByLastName(String lastname) {
+        return employeeMapper.getEmpByLastName(lastname);
     }
 }
