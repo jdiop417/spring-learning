2e73404693e943e121d776fab14bc604141e3d42
yang.chen <jdiop417@gmail.com>
2019-12-26 16:05:57 +0800

    feature:添加CacheConfig

--- springboot-cache/src/main/java/org/learning/springbootcache/service/EmployeeService.java
+++ springboot-cache/src/main/java/org/learning/springbootcache/service/EmployeeService.java
@@ -4,15 +4,13 @@ import org.learning.springbootcache.bean.Employee;
 import org.learning.springbootcache.mapper.EmployeeMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.validation.constraints.NotNull;
 
+@CacheConfig(cacheNames = "emp")
 @Service
 public class EmployeeService {
     Logger logger = LoggerFactory.getLogger(getClass());
@@ -20,20 +18,20 @@ public class EmployeeService {
     @Resource
     private EmployeeMapper employeeMapper;
 
-    @Cacheable(cacheNames = "emp", key = "#id", condition = "#id>0")
+    @Cacheable(/*cacheNames = "emp",*/ key = "#id", condition = "#id>0")
     public Employee getEmpById(Integer id) {
         logger.debug("查询" + id + "号员工");
         return employeeMapper.getEmpById(id);
     }
 
-    @CachePut(value = "emp", key = "#result.id")
+    @CachePut(/*value = "emp",*/ key = "#result.id")
     public Employee updateEmp(Employee employee) {
         logger.debug("更新" + employee.getId() + "号员工");
         employeeMapper.updateEmp(employee);
         return employee;
     }
 
-    @CacheEvict(value = "emp", key = "#id", beforeInvocation = true)
+    @CacheEvict(key = "#id", beforeInvocation = true)
     public void deleteEmp(@NotNull Integer id) {
         logger.debug("删除" + id + "号员工");
         employeeMapper.deletEmpById(id);
@@ -41,10 +39,10 @@ public class EmployeeService {
 
 
     @Caching(
-            cacheable = {@Cacheable(value = "emp", key = "#lastname")},
+            cacheable = @Cacheable(key = "#lastname"),
             put = {
-                    @CachePut(value = "emp", key = "#result.id"),
-                    @CachePut(value = "emp", key = "#result.email")
+                    @CachePut(key = "#result.id"),
+                    @CachePut(key = "#result.email")
             }
     )
     public Employee getEmpByLastName(String lastname) {
