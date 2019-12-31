86f22cfc2f5c7cbfe207350ddc118f68a074a1fc
yang.chen <jdiop417@gmail.com>
2019-12-31 13:51:16 +0800

    提交代码

---
+++ springboot-cache/src/main/java/org/learning/springbootcache/controller/DepartmentController.java
@@ -0,0 +1,21 @@
+package org.learning.springbootcache.controller;
+
+import org.learning.springbootcache.bean.Department;
+import org.learning.springbootcache.service.DepartmentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class DepartmentController {
+
+    @Resource
+    private DepartmentService departmentService;
+
+    @GetMapping("/dept/{id}")
+    public Department getDept(@PathVariable("id") Integer id) {
+        return departmentService.getDeptById(id);
+    }
+}
