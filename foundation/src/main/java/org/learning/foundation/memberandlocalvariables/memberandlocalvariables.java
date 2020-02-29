aa10738bc65adfafd13223ddfda07c6855199ad1
yang.chen <jdiop417@gmail.com>
2020-02-29 17:28:22 +0800

    局部变量与成员变量

---
+++ foundation/src/main/java/org/learning/foundation/memberandlocalvariables/memberandlocalvariables.java
@@ -0,0 +1,30 @@
+package org.learning.foundation.memberandlocalvariables;
+
+public class memberandlocalvariables {
+    static int s;
+    int i;
+    int j;
+
+    {
+        int i = 1;
+        i++;
+        j++;
+        s++;
+    }
+
+    public void test(int j) {
+        j++;
+        i++;
+        s++;
+    }
+
+    public static void main(String[] args) {
+        memberandlocalvariables obj1 = new memberandlocalvariables();
+        memberandlocalvariables obj2 = new memberandlocalvariables();
+        obj1.test(10);
+        obj1.test(20);
+        obj2.test(30);
+        System.out.println(obj1.i + "," + obj1.j + "," + obj1.s);
+        System.out.println(obj2.i + "," + obj2.j + "," + obj2.s);
+    }
+}
