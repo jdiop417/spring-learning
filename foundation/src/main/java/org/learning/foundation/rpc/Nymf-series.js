f780c8909f606ff8bdc810261d86619ecf24bcd9
jdiop417 <1326648048@qq.com>
2024-01-16 22:51:01 +0800

    feat:nymf-series

---
+++ foundation/src/main/java/org/learning/foundation/rpc/Nymf-series.js
@@ -0,0 +1,25 @@
+let obj = JSON.parse($response.body);
+
+if (obj == null) {
+    $done();
+}
+
+for (let i = 0; i < obj.length; i++) {
+    let nymfSeries = obj[i];
+    let photos = nymfSeries.photos;
+
+    if (!photos.length) {
+        continue;
+    }
+
+    for (let i in photos) {
+        let photo = photos[i];
+        if (!photo) {
+            continue;
+        }
+        photo.pro = false;
+        photo.censored = false;
+    }
+}
+
+$done({body: JSON.stringify(obj)});
