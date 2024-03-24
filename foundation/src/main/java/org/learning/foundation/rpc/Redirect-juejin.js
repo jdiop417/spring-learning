d9133cbc121fd3f31b98733ee734f500700badb3
jdiop417 <1326648048@qq.com>
2024-03-24 19:57:44 +0800

    feat:juejin redirect

---
+++ foundation/src/main/java/org/learning/foundation/rpc/Redirect-juejin.js
@@ -0,0 +1,9 @@
+var uri = $request.url;
+let target = decodeURIComponent(uri.substring("https://link.juejin.cn/?target=".length));
+
+var modifiedStatus = 'HTTP/1.1 307 Temporary Redirect';
+var myHeaders = {"Location": target};
+
+var myResponse = {status: modifiedStatus, headers: myHeaders};
+console.log(JSON.stringify(myResponse));
+$done(myResponse);
