79e017ba4dfe44ce5d5784e6c6a1f775ce19e9e0
jdiop417 <1326648048@qq.com>
2024-03-24 16:17:58 +0800

    feat:zhihu redirect

--- foundation/src/main/java/org/learning/foundation/rpc/ZhihuRedirect.js
+++ foundation/src/main/java/org/learning/foundation/rpc/ZhihuRedirect.js
@@ -1,5 +1,4 @@
 let url = $request.url;
-let url="https://link.zhihu.com/?target=https%3A//apps.apple.com/cn/app/anchor-pointer-%25E6%25AD%25A5%25E8%25A1%258C%25E8%25B7%25AF%25E7%25BA%25BF/id791684332%3Fuo%3D4";
 let target = decodeURIComponent(url.substring("https://link.zhihu.com/?target=".length));
 
 const myStatus = "HTTP/1.1 307 Temporary Redirect";
