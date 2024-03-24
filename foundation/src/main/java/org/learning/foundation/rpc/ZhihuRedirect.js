82bc7ff361c21f24d6cb908454d9ebd0723ebfd3
jdiop417 <1326648048@qq.com>
2024-03-24 17:08:52 +0800

    feat:zhihu redirect

--- foundation/src/main/java/org/learning/foundation/rpc/ZhihuRedirect.js
+++ foundation/src/main/java/org/learning/foundation/rpc/ZhihuRedirect.js
@@ -1,9 +1,13 @@
-var uri=$request.url;
+var uri = "https://link.zhihu.com/?target=https%3A//apps.apple.com/cn/app/anchor-pointer-%25E6%25AD%25A5%25E8%25A1%258C%25E8%25B7%25AF%25E7%25BA%25BF/id791684332%3Fuo%3D4";
 let target = decodeURIComponent(uri.substring("https://link.zhihu.com/?target=".length));
 
-const myResponse = {
-    status: "302 Found",
-    headers: {"Location": target}
-};
+const myStatus = "HTTP/1.1 302 Found";
+const myHeaders = {"Location": target};
 
-$done(myResponse);
+const myResponse = {
+    status: myStatus,
+    headers: myHeaders
+};
+console.log(JSON.stringify(myResponse))
+
+$done(JSON.stringify(myResponse));
