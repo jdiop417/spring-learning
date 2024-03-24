f363b570ecc3f0f733ef4a8979447ea629ac6229
jdiop417 <1326648048@qq.com>
2024-03-24 17:51:03 +0800

    feat:zhihu redirect

--- foundation/src/main/java/org/learning/foundation/rpc/ZhihuRedirect.js
+++ foundation/src/main/java/org/learning/foundation/rpc/ZhihuRedirect.js
@@ -1,7 +1,7 @@
 var uri = $request.url;
 let target = decodeURIComponent(uri.substring("https://link.zhihu.com/?target=".length));
 
-var modifiedStatus = 'HTTP/1.1 302 Found';
+var modifiedStatus = 'HTTP/1.1 307 Temporary Redirect';
 var myHeaders = {"Location": target};
 
 var myResponse = {status: modifiedStatus, headers: myHeaders};
