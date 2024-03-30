44f4821fd7237bfe3945d6106e47b627f5ec67ab
jdiop417 <1326648048@qq.com>
2024-03-30 23:07:21 +0800

    feat:51cto redirect

--- foundation/src/main/java/org/learning/foundation/rpc/Redirect-51cto.js
+++
@@ -1,9 +0,0 @@
-var uri = $request.url;
-let target = decodeURIComponent(uri.substring("https://blog.51cto.com/transfer?".length));
-
-var modifiedStatus = 'HTTP/1.1 307 Temporary Redirect';
-var myHeaders = {"Location": target};
-
-var myResponse = {status: modifiedStatus, headers: myHeaders};
-console.log(JSON.stringify(myResponse));
-$done(myResponse);
