20838595c236798f3935faf83e4c11c901caaef4
jdiop417 <1326648048@qq.com>
2024-04-27 13:39:09 +0800

    feat:redirect infoq

---
+++ foundation/src/main/java/org/learning/foundation/rpc/Redirect-infoq.js
@@ -0,0 +1,9 @@
+var uri = $request.url;
+let target = decodeURIComponent(uri.substring("https://www.infoq.cn/link?target=".length));
+
+var modifiedStatus = 'HTTP/1.1 302 Found';
+var myHeaders = {"Location": target};
+
+var myResponse = {status: modifiedStatus, headers: myHeaders};
+console.log(JSON.stringify(myResponse));
+$done(myResponse);
