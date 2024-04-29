1d477de16a2b737d850ad6729ee831d352cfb3f6
jdiop417 <1326648048@qq.com>
2024-04-30 00:00:22 +0800

    feat:nymf-photos

--- foundation/src/main/java/org/learning/foundation/rpc/Nymf-photos.js
+++ foundation/src/main/java/org/learning/foundation/rpc/Nymf-photos.js
@@ -1,13 +1,13 @@
 let obj = JSON.parse($response.body);
-
 if (obj == null) {
     $done();
 }
 
-for (let i = 0; i < obj.length; i++) {
-    let nymfPhoto = obj[i];
-    nymfPhoto.pro = false;
-    nymfPhoto.censored=false;
+for (let photo of obj) {
+    photo.pro = false;
+    if (photo.specifications.includes('Premium')) {
+        photo.specifications = photo.specifications.replace('Premium', 'Free');
+    }
 }
 
 $done({body: JSON.stringify(obj)});
